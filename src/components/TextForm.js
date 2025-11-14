0e2f1740b4bb408b0dee354073b3904b1cfe40e0
Shivangi Ray <shivirai.2985@gmail.com>
2025-11-14 22:38:15 +0530

    Add a Preview button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,8 +5,9 @@ export default function TextForm(props) {
 
   const handleUpClick = ()=>{
     // console.log("upperCase was cliked:"+ text)
-    let newText =text.toUpperCase();
-    setText(newText)
+     let newText= text.toUpperCase();
+     setText(newText)
+   
   }
 
   const handleLoClick = ()=>{
@@ -16,37 +17,74 @@ export default function TextForm(props) {
   }
 
   const handleDltClick = ()=>{
-    // console.log("upperCase was cliked:"+ text)
     let newText =(" ");
     setText(newText)
   }
 
+   const handlePreviewClick = () => {
+   if(text.trim() !==""){
+     setPreviewList([...PreviewList,text]); //new text preview list me add 
+   }
+   
+  };
+
+    const handleSubmitClick = (e) => {
+    e.preventDefault();
+    console.log("Form submitted:", text);
+  };
+
+ 
+
    const handleOnChanged = (event)=>{
     // console.log("On Changed")
     setText(event.target.value)
+    
   }
 
 
   const  [text, setText] = useState('Enter text here');
+   const [PreviewList, setPreviewList] = useState([]); // preview saved text
   // text = ('new text enter') // wrong way to change the state. 
   // setText = ('new text enter') // correct way to change the state
   
     return ( 
         <>
         <h1>{props.heading}</h1>
+
     <div className="mb-3">
-   <textarea className="form-control" value={text} onChange={handleOnChanged} id="myBox" rows="12"></textarea>
+   <textarea className="form-control" 
+   value={text}
+    onChange={handleOnChanged} 
+    id="myBox" rows="12">
+    </textarea>
 </div>
-<button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
-<button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
-<button className="btn btn-primary mx-1" onClick={handleDltClick}> Delete Text</button>
+
+
+<button className="btn btn-primary mx-1" onClick={handleUpClick}> Uppercase</button>
+<button className="btn btn-primary mx-1" onClick={handleLoClick}> Lowercase</button>
+ <button className="btn btn-primary mx-1" onClick={handleDltClick}> Delete </button>
+<button className="btn btn-primary mx-1" type="Preview" onClick={handlePreviewClick}> Save to Preview</button>
+ <button className="btn btn-primary mx-1"  type="submit" onClick={handleSubmitClick}>Submit </button>
+
+
 <div className="container my-3">
-  <h1>Your text summary</h1>
+  <h2>Your text summary</h2>
   <p>{text.split(" ").length} Words and {text.length} Characters</p>
   <p>{0.008 * text.split(" ").length} Minutes Read</p>
-  <h2>Preview</h2>
-  <p>{text}</p>
 
+
+  <h2>Preview (Saved)</h2>
+ {PreviewList.length === 0 ? (
+          <p></p>
+        ) : (
+          <ul>
+            {PreviewList.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+         
+        )}
+         <p>{text}</p>
 </div>
 </>
     )
